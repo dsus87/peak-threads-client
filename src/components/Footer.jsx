b4fb80297ccb077b3626a2b977e8710f3b2e9a02
Daniel Susetio <dsusetio87@gmail.com>
2024-03-23 10:26:30 +0100

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,22 +2,21 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer className="bg-white text-center p-4">
+    <footer className="bg-white text-center p-4 large-margin-top">
       <div className="container">
         <div className="row justify-content-center">
           <div className="col-lg-8 col-md-10 mb-4 mb-md-0">
-            <a href="https://www.linkedin.com/in/daniel-susetio/" style={{ color: 'black', fontSize: 24}}>
+            <a href="https://www.linkedin.com/in/daniel-susetio/" style={{ color: 'black', fontSize: 18}}>
               Developed by Daniel Susetio
             </a>
             <br></br>
-            <a href="https://www.linkedin.com/in/daniel-susetio/" style={{ color: 'black', fontSize: 20, marginRight: '10px' }}>
+            <a href="https://www.linkedin.com/in/daniel-susetio/" style={{ color: 'black', fontSize: 16, marginRight: '10px' }}>
               linkedin    
             </a>
-            <a href="https://github.com/dsus87" style={{ color: 'black', fontSize: 20 }}>
+            <a href="https://github.com/dsus87" style={{ color: 'black', fontSize: 14 }}>
               GitHub   
             </a>
-      
-         
+
 
             <p style={{ color: 'black', fontSize: 16 }}>This site is developed using the MERN stack (MongoDB, Express. js, React, and Node. js, is). 
            Intended for for educational purposes only.</p>
