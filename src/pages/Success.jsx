27d5d69f51160b1fcfe2f9637e0e493a1cb1256c
Daniel Susetio <dsusetio87@gmail.com>
2024-03-28 10:29:39 +0100

    success page

--- src/pages/Success.jsx
+++ src/pages/Success.jsx
@@ -1,6 +1,5 @@
 import React, { useContext, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import axios from 'axios'; // Import Axios
 import { CartContext } from '../context/CartContext';
 import { useAuth } from '../context/AuthContext';
 
@@ -10,46 +9,19 @@ function Success() {
     const { userId, token } = useAuth();
     console.log(token)
 
-    // useEffect(() => {
-    //     if (items.length > 0 && token) {
-    //         const createOrder = async () => {
-    //             try {
-    //                 const orderData = {
-    //                     user: userId,
-    //                     items: items.map(item => ({
-    //                         product: item._id,
-    //                         quantity: item.quantity,
-    //                         price: item.price,
-    //                     })),
-    //                     totalCost: getTotalCost(),
-    //                 };
-
-    //                 console.log(token)
-    //                 await axios.post('https://peak-threads.onrender.com/create', orderData, {
-    //                     headers: {
-    //                         'Content-Type': 'application/json',
-    //                         'Authorization': `Bearer ${token}`,
-    //                     },
-    //                 });
-
-    //                 console.log('Order created successfully');
-    //                 clearCart(); // Clear the cart after successful order creation
-    //                 navigate('/order-success'); // Redirect to an order success page
-    //             } catch (error) {
-    //                 console.error('Error creating order:', error.response ? error.response.data.message : error.message);
-    //                 // Redirect to an error page or show an error message
-    //             }
-    //         };
-
-    //         createOrder();
-    //     } else {
-    //         navigate('/'); // Redirect to home if no items or no token available
-    //     }
-    // }, [items, userId, token, getTotalCost, navigate, clearCart]);
 
     return (
-        <div>
-            <h1>Thank you for your purchase!</h1>
+        <div className="d-flex justify-content-center align-items-center" style={{ height: '50vh' }}>
+            <div className="text-center">
+                <h1>Thank you for your purchase!</h1>
+                <button
+                    style={{ marginTop: '150px', padding: '10px 20px', fontSize: '16px' }}
+                    className="btn btn-dark"
+                    onClick={() => navigate('/')}
+                >
+                   Continue Shopping
+                </button>
+            </div>
         </div>
     );
 }
